4d7f108bf4fc070334f8a8a74e5e156232311d94
tonylinchao <tonyclin@126.com>
2023-06-16 16:37:43 +0800

    change to bot builder

--- app/api/common.ts
+++ app/api/common.ts
@@ -1,6 +1,6 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const OPENAI_URL = "d6q4jkokt5ur6.cloudfront.net";
+export const OPENAI_URL = "api.openai.com";
 const DEFAULT_PROTOCOL = "https";
 const PROTOCOL = process.env.PROTOCOL ?? DEFAULT_PROTOCOL;
 const BASE_URL = process.env.BASE_URL ?? OPENAI_URL;
--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -9,18 +9,11 @@ import {
 } from "@fortaine/fetch-event-source";
 import { prettyObject } from "@/app/utils/format";
 
-interface Overrides {
-  model: string;
-}
-
-const overrides: Overrides = {
-  model: "gpt-4",
-};
-
 export class ChatGPTApi implements LLMApi {
   // public ChatPath = "v1/chat/completions";
-  public ChatPath = "v1/groups/10625773/llm-model/chat";
-  //public ChatPath = "v1/groups/10901293/llm-model/chat"; // Jason's bot
+  // public ChatPath = "v1/groups/10625773/llm-model/chat";
+  // public ChatPath = "v1/groups/10901293/llm-model/chat"; // Jason's bot
+  public ChatPath = "http://127.0.0.1:5000/api/v1/chat";
   public UsagePath = "dashboard/billing/usage";
   public SubsPath = "dashboard/billing/subscription";
 
@@ -33,47 +26,33 @@ export class ChatGPTApi implements LLMApi {
   }
 
   extractMessage(res: any) {
-    // return res.choices?.at(0)?.message?.content ?? "";
-    return res.answer;
+    return res.choices?.at(0)?.message?.content ?? "";
   }
 
   async chat(options: ChatOptions) {
-    const history = options.messages.map((v) => ({
+    const messages = options.messages.map((v) => ({
       role: v.role,
       content: v.content,
     }));
 
-    const modelConfig = {
-      ...useAppConfig.getState().modelConfig,
-      ...useChatStore.getState().currentSession().mask.modelConfig,
-      ...{
-        model: options.config.model,
-      },
-    };
-
     const requestPayload = {
-      history,
-      // stream: options.config.stream,
-      model: modelConfig.model,
-      approach: "rrr",
-      overrides,
-      // temperature: modelConfig.temperature,
-      // presence_penalty: modelConfig.presence_penalty,
+      messages: messages,
     };
 
     console.log("[Request] openai payload: ", requestPayload);
 
-    const shouldStream = !!options.config.stream;
     const controller = new AbortController();
     options.onController?.(controller);
 
     try {
-      const chatPath = this.path(this.ChatPath);
+      const chatPath = this.ChatPath;
       const chatPayload = {
         method: "POST",
         body: JSON.stringify(requestPayload),
         signal: controller.signal,
-        headers: getHeaders(),
+        headers: {
+          "Content-Type": "application/json",
+        },
       };
 
       // make a fetch request
@@ -82,100 +61,11 @@ export class ChatGPTApi implements LLMApi {
         REQUEST_TIMEOUT_MS,
       );
 
-      if (shouldStream) {
-        let responseText = "";
-        let finished = false;
+      const res = await fetch(chatPath, chatPayload);
+      clearTimeout(requestTimeoutId);
 
-        const finish = () => {
-          if (!finished) {
-            options.onFinish(responseText);
-            finished = true;
-          }
-        };
-
-        controller.signal.onabort = finish;
-
-        fetchEventSource(chatPath, {
-          ...chatPayload,
-          async onopen(res) {
-            clearTimeout(requestTimeoutId);
-            const contentType = res.headers.get("content-type");
-            console.log(
-              "[OpenAI] request response content type: ",
-              contentType,
-            );
-
-            if (contentType?.startsWith("text/plain")) {
-              responseText = await res.clone().text();
-              return finish();
-            }
-
-            if (
-              !res.ok ||
-              !res.headers
-                .get("content-type")
-                ?.startsWith(EventStreamContentType) ||
-              res.status !== 200
-            ) {
-              const responseTexts = [responseText];
-              let extraInfo = await res.clone().text();
-              try {
-                const resJson = await res.clone().json();
-                extraInfo = prettyObject(resJson);
-              } catch {}
-
-              if (res.status === 401) {
-                responseTexts.push(Locale.Error.Unauthorized);
-              }
-
-              // 获取 res.answer 参数
-              console.log("请求结果：", responseTexts);
-              const { answer } = await res.clone().json();
-
-              if (extraInfo) {
-                responseTexts.push(answer);
-              }
-
-              responseText = responseTexts.join("\n\n");
-
-              return finish();
-            }
-          },
-          onmessage(msg) {
-            if (msg.data === "[DONE]" || finished) {
-              return finish();
-            }
-            const text = msg.data;
-            console.log("[Response] openai result: ", text);
-            try {
-              const json = JSON.parse(text);
-              // const delta = json.choices[0].delta.content;
-              const delta = json.answer;
-              if (delta) {
-                responseText += delta;
-                options.onUpdate?.(responseText, delta);
-              }
-            } catch (e) {
-              console.error("[Request] parse error", text, msg);
-            }
-          },
-          onclose() {
-            finish();
-          },
-          onerror(e) {
-            options.onError?.(e);
-            throw e;
-          },
-          openWhenHidden: true,
-        });
-      } else {
-        const res = await fetch(chatPath, chatPayload);
-        clearTimeout(requestTimeoutId);
-
-        const resJson = await res.json();
-        const message = this.extractMessage(resJson);
-        options.onFinish(message);
-      }
+      const resJson = await res.json();
+      options.onFinish(resJson.choices[0].message.content);
     } catch (e) {
       console.log("[Request] failed to make a chat reqeust", e);
       options.onError?.(e as Error);
